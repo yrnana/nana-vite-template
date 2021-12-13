81378bd2d25df47490050a2c2c009565729156bc
yrnana <nyryn0945@gmail.com>
2021-12-13 09:14:08 +0900

    feat: tailwindcss 업데이트

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
